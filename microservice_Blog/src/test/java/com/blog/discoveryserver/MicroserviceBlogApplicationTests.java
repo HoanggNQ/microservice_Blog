74aeb3849c9b457779ee529523c4b0d7443d2e4a
ontrip <hoangtoc202@gmail.com>
2025-07-10 20:01:49 +0700

    first commit

---
+++ microservice_Blog/src/test/java/com/blog/discoveryserver/MicroserviceBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
